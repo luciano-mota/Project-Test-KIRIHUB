b9469006a7a390478a5d804731d83cbc5739a02c
Luciano Mota <luciano.mota@luminitisolutions.com>
2021-10-11 21:56:15 -0300

    Iniciando projeto com API com SpringBoot para avaliação da KiriHUB

---
+++ project-test-kirihub/src/main/java/com/projecttestkirihub/ProjectTestKirihubApplication.java
@@ -0,0 +1,13 @@
+package com.projecttestkirihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTestKirihubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTestKirihubApplication.class, args);
+	}
+
+}
---
+++ project-test-kirihub/src/test/java/com/projecttestkirihub/ProjectTestKirihubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecttestkirihub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTestKirihubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
